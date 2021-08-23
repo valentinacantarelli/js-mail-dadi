47636c590e1d1adcc67955b5b462c60637c96348
valentina cantarelli <valentinacanterina@gmail.com>
2021-08-23 10:44:56 +0200

    inserite variabili dadi

--- dadi/script.js
+++ dadi/script.js
@@ -1,20 +1,19 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-var numeroGiocatore = Math.floor(Math.random() * 5) + 1;
-alert(numeroGiocatore + "Ecco il tuo numero");
-console.log(numeroGiocatore)
+alert("Facciamo un gioco. Verra' scelto casualmente un numero tra 1 e 6 per te e per me. Chi ha il numero piu' alto vince.")
+var numeroGiocatore = Math.floor(Math.random() * 7);
+alert("Il tuo numero è " + numeroGiocatore);
 
-var numeroCpu = Math.floor(Math.random() * 5) + 1;
-alert(numeroCpu + "Questo invece è il numero del Computer");
-console.log(numeroCpu)
+
+var numeroCpu = Math.floor(Math.random() * 7);
+alert("Il mio numero invece è " + numeroCpu );
+
 
 // if (numeroGiocatore > numeroCpu) {
-//     alert ("Hai vinto!!");
-// }else if (numeroGiocatore == numeroCpu){
- //   alert("Siete pari!")
-//}else (numeroGiocatore < numeroCpu) {
+//    alert ("Hai vinto!!");
+//  } else if (numeroGiocatore == numeroCpu){
+//   alert("Siete pari!")
+//  } else (numeroGiocatore < numeroCpu) {
 //     alert (" Hai perso!");
-// }
-
-if
+//  }
